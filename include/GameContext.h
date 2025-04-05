8dd292fbaa5b80b613eb480e271a6da912a08f5c
JiangoJ <jiangj2620@gmail.com>
2025-04-05 17:31:26 -0400

    First Commit

---
+++ include/GameContext.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class GameContext {
+
+  /**
+  * [Game Context]: The overarching state of the game tracking all necessary infomration to maintain game functionality.
+  * It is a superset of all Knowledge Contexts (players) along with map based information
+  */
+};
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main() {
+  std::cout << "Start of RTS" << std::endl;
+}
